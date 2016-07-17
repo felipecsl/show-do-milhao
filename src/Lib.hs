f228b884cf62f68104810f2c3a3c065909abda26
Felipe Lima <felipe.lima@gmail.com>
2016-07-16 17:21:07 -0700

    Check the correct answer

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,6 +8,7 @@ import           Data.ByteString.Char8 (lines, pack, singleton, unpack)
 import           Data.Char             (ord)
 import           Data.List             (elemIndex)
 import           Data.List.Split       (splitWhen)
+import           Data.Maybe            (fromMaybe)
 import           Data.String.Utils     (startswith)
 import qualified Data.Text             as T
 import           Data.Text.ICU.Convert (open, toUnicode)
@@ -38,14 +39,18 @@ presentQuestion :: Question -> IO Bool
 presentQuestion q = do
     let stmt = statement q
         opts = options q
-        correct_answer = answer q
+        correctAnswer = answer q
         indexedOpts = mapInd (\x i -> concat ([singleton i] ++ [pack ") "] ++ [x])) opts
     byteStringToString stmt >>= putStrLn
     byteStringToString (intercalate (pack "\n") indexedOpts) >>= putStrLn
     putStrLn "Resposta? "
     answr <- getChar
-    let intAnswr = elemIndex answr ['a'..]
-    return (intAnswr == Just correct_answer)
+    let maybeAnswer = elemIndex answr ['a'..]
+        didAnswerCorrectly = maybeAnswer == Just correctAnswer
+    if didAnswerCorrectly
+      then putStrLn "Certa resposta!"
+      else putStrLn ("Resposta errada. Sua resposta foi " ++ [answr])
+    return didAnswerCorrectly
 
 listToQuestion :: [ByteString] -> Question
 listToQuestion (x:xs) = Question x xs 1
@@ -61,6 +66,16 @@ parseSections bs =
         answers = last groupedQuestionListBySection
     in map (map listToQuestion) (init groupedQuestionListBySection)
 
+takeWhileM :: (a -> IO Bool) -> [a] -> IO [a]
+takeWhileM _ [] = return []
+takeWhileM m (x:xs) = do
+    res <- m x
+    if res
+      then do
+          rest <- takeWhileM m xs
+          return (x : rest)
+      else return [x]
+
 libMain :: IO ()
 libMain = do
     h <- openFile "questions.txt" ReadMode
@@ -69,14 +84,7 @@ libMain = do
     let easy = head sections
     let medium = sections !! 1
     let hard = sections !! 2
-    presentQuestion (easy !! 30) >>= print
+    takeWhileM presentQuestion easy
+    -- presentQuestion (head easy)
+    -- presentQuestion (easy !! 1)
     return ()
-    -- putStrLn $ byteStringToString conv (intercalate (pack "\n") (easy !! 0))
-    -- let questions = [ Question {
-    --       statement = "Em que estado brasileiro nasceu a apresentadora Xuxa?"
-    --     , options = [ "Rio de Janeiro", "Rio Grande do Sul", "Santa Catarina", "Goiás" ]
-    --     , answer = 1
-    --   }]
-    -- result <- presentQuestion (questions !! 0)
-    -- putStrLn (if result)
-    -- return ()
